0ff9e892396832f4da3120231f1bf17ff3ca929e
Romain BERTHON <Romain BERTHON>
2021-06-24 13:54:17 +0200

    session du 24-06-2021

--- TCG-Kata.Tests/TCGTests.fs
+++ TCG-Kata.Tests/TCGTests.fs
@@ -6,7 +6,6 @@ open Swensen.Unquote
 
 [<Fact>]
 let ``Create a game with initial state`` () =
-    
     let game = hydrate [GameCreated]
     test <@ game.Player1 = { Mana = 0; Health = 30; Deck = [0;0;1;1;2;2;2;3;3;3;3;4;4;4;5;5;6;6;7;8]; Hand = [] } @>
     test <@ game.Player2 = { Mana = 0; Health = 30; Deck = [0;0;1;1;2;2;2;3;3;3;3;4;4;4;5;5;6;6;7;8]; Hand = [] } @>
@@ -15,3 +14,21 @@ let ``Create a game with initial state`` () =
 let ``Choose first player as current player`` () =
     let game = hydrate [GameCreated; FirstPlayerChosen Player1]
     test <@ game.Current = Some Player1 @>
+    
+[<Fact>]
+let ``Player should pick cards from his deck for his initial hand`` () =
+    let handInitiatedPlayer1 = HandInitiated {
+        Player = Player1
+        Card1 = 2
+        Card2 = 4
+        Card3 = 5
+    }
+    let handInitiatedPlayer2 = HandInitiated {
+        Player = Player2
+        Card1 = 3
+        Card2 = 1
+        Card3 = 1
+    }
+    let game = hydrate [GameCreated; handInitiatedPlayer1; handInitiatedPlayer2]
+    test <@ game.Player1 = { Mana = 0; Health = 30; Deck = [0;0;1;1;2;2;3;3;3;3;4;4;5;6;6;7;8]; Hand = [2;4;5] } @>
+    test <@ game.Player2 = { Mana = 0; Health = 30; Deck = [0;0;2;2;2;3;3;3;4;4;4;5;5;6;6;7;8]; Hand = [3;1;1] } @>
--- TCG-Kata/TCG.fs
+++ TCG-Kata/TCG.fs
@@ -1,12 +1,22 @@
 module TCG
 
+// https://github.com/bkimminich/kata-tcg
+
+type Card = int
 type PlayerChosen = Player1 | Player2
+
+type HandInitiated = {
+    Player : PlayerChosen
+    Card1 : Card
+    Card2 : Card
+    Card3 : Card
+}
 
 type Evt =
     | GameCreated
     | FirstPlayerChosen of PlayerChosen
+    | HandInitiated of HandInitiated
 
-type Card = int
 type Player = {
     Deck : Card list
     Hand : Card list
@@ -20,11 +30,31 @@ type Game =  {
     Current : PlayerChosen option
 }
 
+let pickCardFromDeck player card =
+    let rec removeFirst deck =
+        match deck with
+        | [] -> []
+        | head::tail when head = card -> tail
+        | head::tail -> head::(removeFirst tail)
+    
+    { player with
+        Hand = player.Hand@[card]
+        Deck = removeFirst player.Deck }
+
 let hydrate (events: Evt list) : Game =  
     
     let handleEvent state evt =
         match evt with
         | FirstPlayerChosen player -> { state with Current = Some player }
+        | HandInitiated handInitiated ->
+            let pickCards player =
+                [handInitiated.Card1; handInitiated.Card2; handInitiated.Card3]
+                |> List.fold pickCardFromDeck player
+            
+            match handInitiated.Player with
+            | Player1 -> { state with Player1 = pickCards state.Player1 }
+            | Player2 -> { state with Player2 = pickCards state.Player2 }
+            
         | _ -> state
     
     let GameCreated::tail = events
